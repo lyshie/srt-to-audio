50389308aa60b79198b83b72a2ac138a87986e52
SHIE, Li-Yi <lyshie@tn.edu.tw>
2021-07-07 14:28:10 +0800

    use ffmpeg to join video and audio

--- join_audio_video.sh
+++ join_audio_video.sh
@@ -1,1 +1,2 @@
-ffmpeg -i video.mp4 -i audio.mp3 -c:v copy -c:a aac output.mp4
+ffmpeg -i video.mp4 -i audio.mp3 -c copy -map 0:v:0 -map 1:a:0 output.mp4
+#ffmpeg -i video.mp4 -i audio.mp3 -c:v copy -c:a aac output.mp4
